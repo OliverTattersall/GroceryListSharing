c854adeaf58d5611104faf2433bc42ae22c445a7
oliver tattersall <oliver.m.tattersall@gmail.com>
2024-11-17 17:25:29 -0500

    initializing spring backend

---
+++ backend/springboot/src/main/java/com/app/GroceryListApp/GroceryListAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.GroceryListApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryListAppApplication.class, args);
+	}
+
+}
