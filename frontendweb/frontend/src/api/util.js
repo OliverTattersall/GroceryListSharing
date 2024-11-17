343d0a72e2437473b22a03fc966e3867b0d0ebbf
oliver tattersall <oliver.m.tattersall@gmail.com>
2024-11-17 17:26:42 -0500

    remove unnecessary files

--- frontendweb/frontend/src/api/util.js
+++
@@ -1,6 +0,0 @@
-
-
-// t is in ms
-export const delay = async (t) => {
-    return new Promise(resolve => setTimeout(resolve, t));
-}
--- frontendweb/frontend/src/components/common/DeleteCross.jsx
+++
@@ -1,8 +0,0 @@
-import './common.css';
-
-
-export const DeleteCross = ({onClick, styles={}}) => {
-    return <button style={styles} className="htmlEntityButton deleteCross" onClick={onClick}>
-        &#88;
-    </button>
-}
